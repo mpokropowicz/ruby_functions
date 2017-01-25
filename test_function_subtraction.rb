56abc18188977451138c641d365ed0227f42a17d
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-01-25 12:26:22 -0500

    float subtraction passes successfully

--- test_function_subtraction.rb
+++ test_function_subtraction.rb
@@ -17,6 +17,13 @@ class TestFunctionsSubtraction < Minitest::Test
 		assert_equal(4, results)
 	end
 
+	def test_pi_minus_one_returns_
+
+		results = subtraction(3.14, 1)
+
+		assert_equal(2.14, results.round(2))
+	end
+
 	def test_String_minus_String
 
 		results = subtraction("Hello", "Bob")
