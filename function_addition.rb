024866814f005c7aa41c80dae58200ddd63acbe5
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-01-25 12:05:11 -0500

    retruns false for non Numeric types

--- function_addition.rb
+++ function_addition.rb
@@ -1,7 +1,13 @@
 def addition(num1, num2)
 
-	raise "paramater num1 must be of Type 'Float' or 'Integer'" unless num1.is_a? Numeric
-	raise "paramater num2 must be of Type 'Float' or 'Integer'" unless num1.is_a? Numeric
+	# raise "paramater num1 must be of Type 'Float' or 'Integer'" unless num1.is_a? Numeric
+	# raise "paramater num2 must be of Type 'Float' or 'Integer'" unless num1.is_a? Numeric
 
-	num1 + num2
+	if (num1.is_a? Numeric) && (num2.is_a? Numeric)
+
+		num1 + num2
+	else
+
+		return false
+	end
 end
