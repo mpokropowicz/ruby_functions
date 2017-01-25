58f1f094ced87e261193926e8522d0640e35c707
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-01-25 15:34:36 -0500

    commented inject method

--- function_addition.rb
+++ function_addition.rb
@@ -6,28 +6,29 @@ def addition(num1, num2, *nums)
 		    nums.all? {|n| n.is_a? Numeric} ||
 	   	   (num1.class == String && num2.class == String && 
 	   	    nums.all? {|n| n.class == String})
+
 		return false
 	end
 
-	if num1.class == String && num2.class == String ||
-	   (num1.class == String && num2.class == String && 
-	   	nums.all? {|n| n.class == String})
+	if num1.class == String && num2.class == String && 
+	   	nums.all? {|n| n.class == String}
 
 	   	resultS = num1 + num2
 
-		nums.each do |n|
+	   	nums.each do |n|
 
 			resultS += n
 		end
 
 		resultS
+		#resultS += nums.inject { |sum, value| sum += value}
 	else
 
 		result = 0
 	
 		nums.each do |n|
 
-			result += nums[n-1]
+			result += n
 		end
 
 	    result = result + num1 + num2
