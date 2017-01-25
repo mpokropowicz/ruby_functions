78447f5cce2eec6d3da7e0f4af66dd982c22b06e
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-01-25 11:31:41 -0500

    throws proper errors for non float and int types

--- function_addition.rb
+++ function_addition.rb
@@ -1,4 +1,7 @@
 def addition(num1, num2)
 
+	raise "paramaters must be of Type 'Float' or 'Integer'" if (num1.class != Float && num1.class != Integer)
+	raise "paramaters must be of Type 'Float' or 'Integer'" if (num2.class != Float && num2.class != Integer)
+
 	num1 + num2
 end
