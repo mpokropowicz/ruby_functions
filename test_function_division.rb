c12103971e18025cf0a1c2feaecf039c33c325da
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-01-26 22:29:16 -0500

    test_1_by_0_returns_0 fails

---
+++ test_function_division.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require_relative "function_division.rb"
+
+class TestFunctionDivision < Minitest::Test
+
+	def test_1_by_0_returns_0
+
+		assert_equal(0, division(1, 0))
+	end
+end
